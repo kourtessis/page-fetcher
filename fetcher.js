73c0866b1c3d826678be64e8cba1631adeaff7fb
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-23 19:35:10 -0800

    updated notes via John

--- fetcher.js
+++ fetcher.js
@@ -1,27 +1,27 @@
 ///TERMINAL INPUT: node fetcher.js http://www.example.edu/ ./index.html
 
-const URL = process.argv[2];  // Grab varaibles: Index 0 = website, index 1 = filepath
-const filePath = process.argv[3];
+const URL = process.argv[2];  // URL from argv index 2
+const filePath = process.argv[3]; // URL from argv index 3
 const fs = require('fs');
 const request = require('request');
-//downloading website
-
 
 
 request(URL, (error, response, body) => {
-  let data = error + response + body;
+  let data = error + response + body; // Adding webpage elements to text file.
     
-  fs.writeFile(filePath, data, (err) => {
+  fs.writeFile(filePath, data, (err) => { // Create text file
       
     if (err)
-      console.log(err);
+      console.log(err); //display if error in page
   
     else {
-      fs.readFile(filePath, 'utf8', (error, data) => {
+      fs.readFile(filePath, 'utf8', (error, data) => { // Read new file for purpose of getting data.length === size in bytes
         if (!error) {
-          console.log(`Downloaded and saved ${data.length} bytes to ${filePath}`);
+          console.log(`Downloaded and saved ${data.length} bytes to ${filePath}`); //Output print to display filesize and path
         }
       });
     }
   });
 });
+
+//  OUTPUT EXAMPLE: Downloaded and saved 1275 bytes to ./index.html
